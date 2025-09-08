d7df439005e6105c0c07e5aa0bd92b475e50aa83
Pierre-12-enzo <dusenge.enzo87@gmail.com>
2025-09-08 05:48:31 -0700

    modified

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const auth = require('../middleware/auth'); // JWT middleware
+const auth = require('../Middleware/auth'); // JWT middleware
 
 
 // Traditional Signup
